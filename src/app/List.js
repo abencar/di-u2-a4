8ad960e6987dc2f49f28ca9d3dd72ccaca1ef286
Álvaro Benítez Carmona <76166064+abencar@users.noreply.github.com>
2024-11-04 11:51:48 +0100

    nose

--- src/app/List.js
+++ src/app/List.js
@@ -1,24 +1,51 @@
 import { people } from './data.js';
 import { getImageUrl } from './utils.js';
 
-export default function List() {
-  const listItems = people.map(person =>
-    <li key={person.id}>
-      <img
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
+export default function Componente () {
+    return (
+      <div>
+        <ListQuimicos />
+        <List />
+      </div>
+    );
+  }
+export function List() {
+  const listItems = people
+    .filter(person => person.profession !== 'químico')
+    .map(person => (
+      <ListItem key={person.id} person={person} />
+    ));
+  return (
+    <article>
+      <h1>Científicos</h1>
+      <ul>{listItems}</ul>
+    </article>
+  );
+}
+
+export function ListQuimicos() {
+  const listItems = people
+    .filter(person => person.profession === 'químico')
+    .map(person => (
+      <ListItem key={person.id} person={person} />
+    ));
+  return (
+    <article>
+      <h1>Químicos</h1>
+      <ul>{listItems}</ul>
+    </article>
+  );
+}
+
+function ListItem({ person }) {
+  return (
+    <li>
+      <img src={getImageUrl(person)} alt={person.name} />
       <p>
         <b>{person.name}:</b>
         {' ' + person.profession + ' '}
         conocido/a por {person.accomplishment}
       </p>
     </li>
-  );
-  return (
-    <article>
-      <h1>Científicos</h1>
-      <ul>{listItems}</ul>
-    </article>
   );
 }
--- src/app/RecipeList.js
+++ src/app/RecipeList.js
@@ -4,6 +4,27 @@ export default function RecipeList() {
   return (
     <div>
       <h1>Recetas</h1>
+      {receta(recipes)}
+    </div>
+
+  );
+}
+
+export function receta(recipes){
+  return (
+    <div>
+      {recipes.map(recipe =>
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {recipe.ingredients.map(ingredient =>
+              <li key={ingredient}>
+                {ingredient}
+              </li>
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
